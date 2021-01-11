d8d14a693d9cdd9599c3738b28944f10308214a1
Jens Lerche <jlerche@saba.com>
2021-01-11 12:07:13 +0100

    Methode Addiere hinzugefügt

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -10,22 +10,33 @@ namespace Taschenrechner_Iteration_1
     {
         static void Main(string[] args)
         {
+            // User Story "Addieren". Als Benutzer möchte ich 2 Zahlen eingeben und addieren.
             Console.Write("Bitte ersten Summanden eigeben: ");
             string ersterSummand = Console.ReadLine();
             Console.Write("Bitte zweiten Summanden eigeben: ");
             string zweiterSummand = Console.ReadLine();
             
-
             // Wandel Text in Ganzzahlen
             double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
             double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
 
-            // Berechnung
-            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            // Addiere
+            double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl);
 
             //Ausgabe
             Console.WriteLine("Summe: {0}", summe);
+            WarteAufEingabe();
+
+        }
+        static void WarteAufEingabe()
+        {
+            Console.WriteLine("Drücke Return zum Beenden der Anwendung!");
             Console.ReadLine();
+        }
+        static double Addiere(double ersterSummandAlsZahl, double zweiterSummandAlsZahl)
+        {
+            double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
+            return summe;
         }
     }
 }
