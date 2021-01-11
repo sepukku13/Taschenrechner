8e621d4b67c13e1b2c9408756295563c669f92b9
Jens Lerche <jlerche@saba.com>
2021-01-11 16:57:17 +0100

    If Operator implementiert

--- Taschenrechner/Program.cs
+++ Taschenrechner/Program.cs
@@ -7,36 +7,53 @@ using System.Threading.Tasks;
 namespace Taschenrechner_Iteration_1
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            // User Story "Addieren". Als Benutzer möchte ich 2 Zahlen eingeben und addieren.
-            Console.Write("Bitte ersten Summanden eigeben: ");
-            string ersterSummand = Console.ReadLine();
-            Console.Write("Bitte zweiten Summanden eigeben: ");
-            string zweiterSummand = Console.ReadLine();
+            double resultat = 0;
             
+            // User Story "Addieren". Als Benutzer möchte ich 2 Zahlen eingeben und addieren.
+            string ersterWert = HoleBenutzerEingabe ("Bitte erste Zahl eigeben: ");
+            string zweiterWert = HoleBenutzerEingabe("Bitte zweite Zahl eigeben: ");
+            string operation = HoleBenutzerEingabe("Bitte Operator eingeben (+ / -): ");
+
             // Wandel Text in Ganzzahlen
-            double ersterSummandAlsZahl = Convert.ToDouble(ersterSummand);
-            double zweiterSummandAlsZahl = Convert.ToDouble(zweiterSummand);
+            // TODO: Auslagern in eine Methode
+            double ersteZahl = Convert.ToDouble(ersterWert);
+            double zweiteZahl = Convert.ToDouble(zweiterWert);
 
-            // Addiere
-            double summe = Addiere(ersterSummandAlsZahl, zweiterSummandAlsZahl);
-
-            //Ausgabe
-            Console.WriteLine("Summe: {0}", summe);
-            WarteAufEingabe();
-
+            // Berechnung
+            if (operation == "+")
+            {
+                resultat = Addiere(ersteZahl, zweiteZahl);
+                Console.WriteLine("Ergebnis: {0}", resultat);
+            }
+            else if (operation == "-")
+            {
+                resultat = Subtrahiere(ersteZahl, zweiteZahl);
+                Console.WriteLine("Ergebnis: {0}", resultat);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe!");
+            }
+            HoleBenutzerEingabe("Drücke Return zum Beenden der Anwendung!");
         }
-        static void WarteAufEingabe()
+        static string HoleBenutzerEingabe (string eingabetext)
         {
-            Console.WriteLine("Drücke Return zum Beenden der Anwendung!");
-            Console.ReadLine();
+            Console.Write(eingabetext);
+            string summand = Console.ReadLine();
+            return summand;
         }
         static double Addiere(double ersterSummandAlsZahl, double zweiterSummandAlsZahl)
         {
             double summe = ersterSummandAlsZahl + zweiterSummandAlsZahl;
             return summe;
+        }
+        static double Subtrahiere(double minuend, double subtrahend)
+        {
+            double differenz = minuend - subtrahend;
+            return differenz;
         }
     }
 }
